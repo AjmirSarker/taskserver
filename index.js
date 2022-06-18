dff5aaaa6191ae665d199cb8377516d0daf16ebd
AjmirSarker <arafat15-13512@diu.edu.bd>
2022-06-18 18:36:06 +0600

    final

--- index.js
+++ index.js
@@ -11,13 +11,91 @@ app.use(express.json());
 
 
 
-// const uri = "mongodb+srv://job123:<password>@cluster0.ta7rt.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = "mongodb+srv://job123:tdSDkWphgUv1ku1k@cluster0.ta7rt.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 // client.connect(err => {
 //   const collection = client.db("test").collection("devices");
 //   // perform actions on the collection object
 //   client.close();
 // });
+async function run(){
+  try{
+    await client.connect();
+    const profileCollection =client.db('jobtask').collection('profile');
+    console.log('db connected');
+    const projectCollection =client.db('jobtask').collection('projects');
+
+    //get profile
+  //   app.get('/profile', async (req, res) => {
+  //     const query = {};
+  //     const cursor = profileCollection.find(query);
+  //     const items = await cursor.toArray();
+  //     res.send(items);
+  // });
+//   app.get('/users/:email', async (req, res) => {
+//     const email = req.params.email;
+//     const query = {email: email};
+//     const cursor = userCollection.find(query);
+//     const reviews = await cursor.toArray();
+//     res.send(reviews);
+
+// });
+//get profile by id
+app.get('/profile/:email', async (req, res) => {
+  const email = req.params.email;
+  const query = { email: email };
+  const result = await profileCollection.findOne(query);
+  res.send(result);
+
+});
+
+//put profile
+app.put('/profile/:email', async (req, res) => {
+  const email = req.params.email;
+  const user = req.body;
+  const filter = { email: email };
+  const options = { upsert: true };
+  const updateDoc = {
+    $set: user,
+  };
+  const result = await profileCollection.updateOne(filter, updateDoc, options);
+  
+  res.send( result);
+})
+
+//projects get all
+app.get('/projects',async(req,res)=>{
+  const users = await projectCollection.find().toArray()
+  res.send(users)
+})
+//get by id project
+app.get('/projects/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: ObjectId(id) };
+  const result = await projectCollection.findOne(query);
+  res.send(result);
+
+});
+//project post
+app.post('/projects', async (req, res) => {
+  const newProject = req.body;
+  const result = await projectCollection.insertOne(newProject);
+  res.send(result);
+});
+//project delete by id
+app.delete('/projects/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: ObjectId(id) };
+  const result = await projectCollection.deleteOne(query);
+  res.send(result); 
+});
+
+
+  }finally{
+
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
